0bb963ee9bdf0906831f077257967e3e8dc49b4f
baristp04 <baristepe2004@yandex.com>
2025-02-24 18:29:03 +0300

    fix

--- modules/display.nix
+++ modules/display.nix
@@ -5,7 +5,10 @@
       enable = true;
     };
     displayManager = {
-      sddm.enable = true;
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
     };
     desktopManager = {
       plasma6.enable = true;
