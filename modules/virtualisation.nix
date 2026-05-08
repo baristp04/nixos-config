cfad1c3c7353b0d427ec14f2bc5b0ca32ed4c5eb
baristp04 <baristepe2004@yandex.com>
2026-05-08 10:56:04 +0300

    moved to system config

---
+++ modules/virtualisation.nix
@@ -0,0 +1,16 @@
+{
+   virtualisation = {
+    virtualbox = {
+      host = {
+        enable = true;
+        enableExtensionPack = true;
+      };
+      guest = {
+        enable = true;
+        dragAndDrop = true;
+      };
+    };
+   };
+
+   users.extraGroups.vboxusers.members = [ "btepe" ];
+}
